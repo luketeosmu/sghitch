d6b208af754ebc2193adc531d5f8e36785baff9f
Shaun Ting <77608416+ststheting@users.noreply.github.com>
2022-10-04 16:58:08 +0800

    more formatting to newreq

--- src/views/NewReq.vue
+++ src/views/NewReq.vue
@@ -2,26 +2,33 @@
     <div>
         <!-- starting point should be taken from google maps - should return a longitude/latitude -->
         <!-- when searching for requests, sort by distance away from starting point -->
-        <form>
-            <div className="flex form-control">
-                <div className="justify-center">
-                    <input v-model.lazy="input.s_address" type="text" placeholder="Starting Point e.g. Tampines" className="input input-bordered input-warning w-full max-w-md mt-5" />
+        <Nav/>
+        <div class="container mx-auto">
+            <form>
+                <div className="flex form-control text-center mb-10">
+                    <div className="justify-center">
+                        <input v-model.lazy="input.s_address" type="text" placeholder="Starting Point e.g. Tampines" className="input input-bordered input-warning w-full max-w-md mt-5" />
+                    </div>
+                    <div class="justify-center">
+                        <input v-model.lazy="input.d_address" type="text" placeholder="Destination e.g. Woodlands" className="input input-bordered input-warning w-full max-w-md mt-5" />
+                    </div>
                 </div>
-                <div class="justify-center">
-                    <input v-model.lazy="input.d_address" type="text" placeholder="Destination e.g. Woodlands" className="input input-bordered input-warning w-full max-w-md mt-5" />
+                <div class="text-center">
+                    <button type="button" @click='queryMapsStart()' class="btn btn-warning bg-yellow-300 text-black">Submit</button>
                 </div>
-                <button type="button" @click='queryMapsStart()'>Submit</button>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </template>
 <script>
 import MapsService from '../services/MapsService'
+import Nav from '../components/Nav.vue'
 
 export default {
     name: "NewReq",
     components: {
         MapsService,
+        Nav
     },
     props: {},
     data () {
