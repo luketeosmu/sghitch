6133b6333ba52a231808b733ecae4acc4ee0e4e9
luketeosmu <luke.teo.2020@scis.smu.edu.sg>
2022-10-06 16:22:09 +0800

    added routing to nav bar & minimize feature to favourites page

--- src/components/Favourite.vue
+++ src/components/Favourite.vue
@@ -1,18 +1,18 @@
 <template lang="">
     <div>
-        <a href="#" class="relative mx-3 p-3 pt-12 bg-yellow-100 rounded-2xl flex justify-center hover:bg-yellow-200">
-                <h3 class="absolute top-0 font-bold text-2xl font-sans mb-3 ml-5 text-center">{{ favourites[0] }}</h3>
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-                    <Request />
-                    <Request />
-                    <Request />
-                    <Request />
-                    <Request />
-                    <Request />
-                    <Request />
-                    <Request />
+        <div v-if='favourites.length != 0' v-for="favourite in favourites">
+            <a class="relative mb-5 mx-3 p-3 pt-12 bg-yellow-100 rounded-2xl flex justify-center hover:bg-yellow-200 cursor-pointer" >
+                <h3 class="absolute top-0 font-bold text-2xl font-sans mb-3 ml-5 text-center" id="favourite">{{ favourite }}</h3>
+                <button class="btn btn-square btn-xs btn-warning absolute top-2 right-5 font-black" @click="minimize(favourite)"> X </button>
+                <div :id="favourite">
+                    <Request :favourite="favourite" />
                 </div>
             </a>
+        </div>
+        <div v-else class="flex justify-center items-center"> 
+            <h3 class="text-center text-2xl">No favourites to display</h3>
+            <button @click='addFavourite()' type="button" class="block bg-yellow-300 p-3 ml-5 rounded-2xl text-black font-semibold" to="newReq">Add Favourite</button>
+        </div>
     </div>
 </template>
 <script>
@@ -25,11 +25,23 @@ export default {
     },
     components: {
         Request,
-
     },
     data() {
         return {
-            favourites: ["Woodlands to Tampines"]
+            favourites: ["Woodlands to Tampines", "SMU to Woodlands"],
+            
+            //GET array of request objects and use v-for to show individual requests
+            // favourites: []
+        }
+    },
+    methods: {
+        minimize(favourite) {
+            var div = document.getElementById(favourite)
+            if (div.style.display === "none") {
+                div.style.display = "block";
+            } else {
+                div.style.display = "none";
+            }
         }
     }
 }
--- src/components/Request.vue
+++ src/components/Request.vue
@@ -1,33 +1,100 @@
 <template lang="">
     <div>
-        <a href="#" class="card relative p-6 mx-5 mb-5 w-72 h-44 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-red-200">
-            <ul class="text-lg font-bold mb-3">
-                <li>Time: {{ time }}</li>
-                <li>Pax: {{ pax }} persons</li>
-                <li class="font-normal text-sm">Hitcher: {{ hitcher }}</li>
-            </ul>
-            <div class="inline-flex items-center mb-5">
-                <button href="#" class= " py-2 px-3 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                            From: {{ from }}
-                </button>
-                <svg aria-hidden="true" class="mx-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                <button href="#" class= " py-2 px-3 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                        To: {{ to }}
-                </button>
+        <div v-if="requests.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+            <div  v-for="request in requests">
+                <a class="card relative p-6 mx-5 mb-5 w-72 h-44 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-red-200">
+                    <ul class="text-lg font-bold mb-3">
+                        <li>Time: {{ request.time}}</li>
+                        <li>Pax: {{ request.pax }} persons</li>
+                        <li class="font-normal text-sm">Hitcher: {{ request.hitcher }}</li>
+                    </ul>
+                    <div class="inline-flex items-center mb-5">
+                        <button href="#" class= " py-2 px-3 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                    From: {{ request.from }}
+                        </button>
+                        <svg aria-hidden="true" class="mx-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        <button href="#" class= " py-2 px-3 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                To: {{ request.to }}
+                        </button>
+                    </div>
+                </a>
             </div>
-        </a>
+        </div>
+        <div v-else class="text-center justify-center items-center">
+            <span class="text-center text-xl">No current ride requests</span>
+        </div>
     </div>
 </template>
 <script>
 export default {
     name: "Request",
+    props: {
+        favourite: String
+    },
+    components: {
+    },
     data() {
         return {
-            hitcher: "Luke Teo",
-            time: "08:00",
-            pax: 2,
-            from: "730308",
-            to: "529498"
+            //use Favourite to GET all request from "Woodlands to Tampines"
+            // requests: [],
+            requests: [
+                    {
+                        hitcher: "Luke Teo",
+                        time: "08:00",
+                        pax: 2,
+                        from: "730308",
+                        to: "529498"
+                    },
+                    {
+                        hitcher: "Shaun Ting",
+                        time: "10:00",
+                        pax: 3,
+                        from: "730308",
+                        to: "529498"
+                    },
+                    {
+                        hitcher: "Ali baba",
+                        time: "12:00",
+                        pax: 4,
+                        from: "730308",
+                        to: "529498"
+                    },
+                    {
+                        hitcher: "John Wick",
+                        time: "12:00",
+                        pax: 4,
+                        from: "730308",
+                        to: "529498"
+                    },
+                    {
+                        hitcher: "Jennie Kim",
+                        time: "12:00",
+                        pax: 4,
+                        from: "730308",
+                        to: "529498"
+                    },
+                    {
+                        hitcher: "KIm Jong Kook",
+                        time: "12:00",
+                        pax: 4,
+                        from: "730308",
+                        to: "529498"
+                    },
+                    {
+                        hitcher: "Kimchi Jigae",
+                        time: "12:00",
+                        pax: 4,
+                        from: "730308",
+                        to: "529498"
+                    },
+                    {
+                        hitcher: "Buddae Jigae",
+                        time: "12:00",
+                        pax: 4,
+                        from: "730308",
+                        to: "529498"
+                    },
+            ]
         }
     }
 }
