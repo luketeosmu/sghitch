26c3ad33da4970e8f80fb088f6a479c5a71f3a40
timus tay <111484493+TimusAA@users.noreply.github.com>
2022-10-13 00:18:55 +0800

    test

--- src/views/RegisterOTP.vue
+++ src/views/RegisterOTP.vue
@@ -3,7 +3,7 @@
       <div class="absolute z-30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  ">
           <div class="card w-96 p-12 bg-yellow-50">
           <form>
-            <h1 class="text-gray-800 font-bold font-sans text-2xl mb-7 text-center"> We've sent a verification code to your email </h1>
+            <h1 class="text-gray-800 font-bold font-sans text-2xl mb-7 text-center"> We've sent a verification code to your mobile - 98xxxx32 </h1>
             <div class="flex items-center border-2 py-2 px-3 mb-5 rounded-2xl border-gray-500">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
