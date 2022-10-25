82d8dedc100f1b0dd1c15e1d528aa601a8397d5b
Shaun Ting <77608416+ststheting@users.noreply.github.com>
2022-10-25 15:32:43 +0800

    minor test

--- src/main.js
+++ src/main.js
@@ -136,30 +136,30 @@ const router = createRouter({
     }
 })
 
-const getCurrentUser = () => {
-    return new Promise ((resolve, reject) => {
-        const removeListener = onAuthStateChanged (
-            getAuth(),
-            (user) => {
-                removeListener();
-                resolve(user)
-            },
-            reject
-        )
-    })
-}
+// const getCurrentUser = () => {
+//     return new Promise ((resolve, reject) => {
+//         const removeListener = onAuthStateChanged (
+//             getAuth(),
+//             (user) => {
+//                 removeListener();
+//                 resolve(user)
+//             },
+//             reject
+//         )
+//     })
+// }
 
-router.beforeEach(async (to, from, next) => {
-    if (to.matched.some((record) => record.meta.requiresAuth)) {
-        if(await getCurrentUser()) {
-            next()
-        } else {
-            next("/login")
-        }
-    } else {
-        next()
-    }
-})
+// router.beforeEach(async (to, from, next) => {
+//     if (to.matched.some((record) => record.meta.requiresAuth)) {
+//         if(await getCurrentUser()) {
+//             next()
+//         } else {
+//             next("/login")
+//         }
+//     } else {
+//         next()
+//     }
+// })
 
 createApp(App).use(VueGoogleMaps, {
     load: {
