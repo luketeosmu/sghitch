af4f88f75be1079c550ccdb0c1e532eb30ec880f
Shaun Ting <77608416+ststheting@users.noreply.github.com>
2022-10-27 12:58:47 +0800

    fixed background

--- src/views/AccountSettings.vue
+++ src/views/AccountSettings.vue
@@ -1,5 +1,5 @@
 <template lang="">
-    <div class="drawer bg-no-repeat bg-cover bg-bottom" style="background-image: url('../src/images/settingsBG.jpg')">
+    <div class="drawer bg-no-repeat bg-cover bg-bottom bg-settings-background">
          <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
          <div class="drawer-content flex flex-col">
              <!-- Navbar -->
