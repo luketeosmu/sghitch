bce2109e5f6507e9f27ee0e29cec3e3182838309
luketeosmu <luke.teo.2020@scis.smu.edu.sg>
2022-11-13 16:33:59 +0800

    reject offer

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -83,6 +83,22 @@
                             Ride with {{ acceptedOffer.displayName }} is about to commence!
                         </div>
                     </li>
+                    <li v-if="this.rejectedOffer != null">
+                        <label v-if="this.auth.currentUser.displayName == this.rejected.displayName" class="hover:bg-slate-500 active:bg-slate-500 text-white" >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                            </svg>
+                            Ride with {{ rejectedOffer.driverName }} is rejected
+                            <button @click="removeRejected()">delete</button>
+                        </label>
+                        <div v-else class="hover:bg-slate-500 active:bg-slate-500 text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                            </svg>
+                            Ride with {{ rejectedOffer.displayName }} is rejected
+                            <button @click="removeRejected()">delete</button>
+                        </div>
+                    </li>
                     <li v-if="this.offers.length != 0 && this.acceptedOffer == null" v-for="offer of this.offers">
                         <label v-if="offer.status == 'pending'" :for="offer.oid" class="hover:bg-slate-500 active:bg-slate-500 text-white"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -257,6 +273,7 @@ export default {
                 imageUrl: null
             },
             acceptedOffer: null,
+            rejectedOffer: null,
             driverOffers: [],
             hitcherOffers: [],
             offers: [
@@ -412,8 +429,23 @@ export default {
         }).catch((error) => {
             console.error(error);
         });
+        get(child(dbRef, `userInfo/${auth.currentUser.uid}`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            this.rejectedOffer = snapshot.val().rejectedOffer
+        } else {
+            console.log("No data available");
+        }
+        }).catch((error) => {
+            console.error(error);
+        });
     },
     methods: {
+        removeRejected() {
+            const db = getDatabase();
+            const auth = getAuth()
+            set(ref(db, 'userInfo/' + auth.currentUser.uid + '/rejectedOffer'), null);
+            this.rejectedOffer = null
+        },
         rideStart(offer) {
             // this.acceptedOffer = null
             const db = getDatabase();
@@ -426,6 +458,12 @@ export default {
                 console.log(error)
             })
             remove(ref(db, 'hitcherOffers/' + offer.requesterId)).catch((error)=> {
+                console.log(error)
+            })
+            remove(ref(db, 'driverOffers/' + offer.requesterId)).catch((error)=> {
+                console.log(error)
+            })
+            remove(ref(db, 'hitcherOffers/' + auth.currentUser.uid)).catch((error)=> {
                 console.log(error)
             })
         },
@@ -470,7 +508,13 @@ export default {
             // console.log(offer.oid)
         },
         declineOffer(offer) {
-
+            remove(ref(db, 'driverOffers/' + auth.currentUser.uid + '/' + offer.oid)).catch((error)=> {
+                console.log(error)
+            })
+            remove(ref(db, 'hitcherOffers/' + offer.requesterId + '/' + offer.oid)).catch((error)=> {
+                console.log(error)
+            })
+            set(ref(db, 'userInfo/' + offer.uid + '/rejectedOffer'), offer);
         },
         getPendingCount() {
             let count = 0
